22860a8de4328ec63eb1502035efc9c03087a985
Maximiliano Correa <jmaxicorrea@gmail.com>
2024-10-25 12:11:22 -0300

    feat: Query current time Feature

--- src/main/java/com/maxicorrea/github/java_spring_boot_semantic_versioning/SemanticVersioningController.java
+++ src/main/java/com/maxicorrea/github/java_spring_boot_semantic_versioning/SemanticVersioningController.java
@@ -3,6 +3,7 @@ package com.maxicorrea.github.java_spring_boot_semantic_versioning;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.time.*;
 
 @RestController
 @RequestMapping("/version")
@@ -12,5 +13,10 @@ public class SemanticVersioningController {
     public String getVersion() {
         return "Semantic Versioning"; 
     }
-        
+
+    @GetMapping("/time")
+    public String getMethodName() {
+        return String.valueOf(LocalDateTime.now());
+    }
+    
 }
